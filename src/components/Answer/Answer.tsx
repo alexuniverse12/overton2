a40f08830606a1d526982c1b7ff4a18a48c9010d
Alexander <alexander@Alexanders-MacBook-Air.local>
2022-10-03 01:14:30 +0200

    sda

--- src/components/Answer/Answer.tsx
+++ src/components/Answer/Answer.tsx
@@ -1,4 +1,6 @@
 
+import axios from "axios"
+import { useTonhubConnect } from "react-ton-x"
 import "./Answer.css"
 
 export type AnswerProps = {
@@ -9,59 +11,44 @@ export type AnswerProps = {
 
 
 const Answer = ({ title, answerText, date }: AnswerProps) => {
-    const giveReward = function (e: any) {
-        // const tonData = JSON.parse(localStorage.getItem("connection") || "")
-        // let today = new Date();
-        // let dd = String(today.getDate()).padStart(2, '0');
-        // let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        // let yyyy = today.getFullYear();
-        // console.log(formData)
-        // const rightDate = mm + '/' + dd + '/' + yyyy;
-        // if (inputFields.type === "submitQuestion") {
-        //     console.log("TEST")
-        //     const tonData = JSON.parse(localStorage.getItem("connection") || "")
-        //     const questionsCollection = collection(db, "questions");
-        //     if (tonData) {
-        //         console.log("TEST 2")
-
-        //         try {
-        //             const response: any = await axios({
-        //                 method: 'post',
-        //                 url: "http://104.248.100.22:3000/deploy",
-        //                 data: {
-        //                     owner: tonData.walletConfig.address,
-        //                 }
-        //             });
-        //             const responseData = response.data
-        //             const reqTrans = await connect.api.requestTransaction({
-        //                 to: responseData.contractAddress,
-        //                 value: (parseFloat(formData.rewardAmount) * 1e9).toString(),
-        //                 stateInit: responseData.stateInit,
-        //                 text: "Smart contract deployment",
-        //                 payload: responseData.payload
-        //             })
-        //             console.log(reqTrans)
-
-        //             await addDoc(questionsCollection, {
-        //                 userID: tonData.walletConfig.address,
-        //                 title: formData.questionTitle,
-        //                 questionText: formData.questionDescription,
-        //                 rewardAmount: formData.rewardAmount,
-        //                 date: rightDate
-        //             });
-        //         } catch (error) {
-        //             console.error(error, "IDI NAXUI");
-        //         }
-
-        //     }
+    const connect = useTonhubConnect()
+    const giveReward = async () => {
+        const tonData = JSON.parse(localStorage.getItem("connection") || "")
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let yyyy = today.getFullYear();
+        const rightDate = mm + '/' + dd + '/' + yyyy;
+        if (tonData) {
+            try {
+                const response: any = await axios({
+                    method: 'post',
+                    url: "http://104.248.100.22:3000/withdraw",
+                    data: {
+                        send_to_address: tonData.walletConfig.address,
+                    }
+                });
+                const responseData = response.data
+                const reqTrans = await connect.api.requestTransaction({
+                    to: responseData.contractAddress,
+                    value: (0.01 * 1e9).toString(),
+                    stateInit: responseData.stateInit,
+                    text: "Smart contract deployment",
+                    payload: responseData.payload
+                })
+                console.log(reqTrans)
+            } catch (error) {
+                console.error(error, "IDI NAXUI");
+            }
 
 
 
 
-        //     // console.log("user set nickname and languages: ", uid);
-        //     // dispatch(getCustomUserInfo(uid))
-        //     // navigate("/orders")
-        // }
+
+            // console.log("user set nickname and languages: ", uid);
+            // dispatch(getCustomUserInfo(uid))
+            // navigate("/orders")
+        }
     }
 
     return (
@@ -71,7 +58,7 @@ const Answer = ({ title, answerText, date }: AnswerProps) => {
 
             <div className="answerFooter">
                 <p className="answerDate">{date}</p>
-                <button onClick={giveReward} className="giveRewardBtn">Give reward</button>
+                <button onClick={() => giveReward()} className="giveRewardBtn">Give reward</button>
             </div>
         </div>
 
